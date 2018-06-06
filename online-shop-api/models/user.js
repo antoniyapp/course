643e454427022b43bc0a57ddb80601982af9ae28
Pavel Koev <pavelspr1@abv.bg>
2018-06-06 03:18:19 +0300

    modified model

--- online-shop-api/models/user.js
+++ online-shop-api/models/user.js
@@ -4,36 +4,41 @@ const UserSchema = new mongoose.Schema(
     {
       username: {
         type: String,
-        required: [true, "Username required!"],
+        required: [true, "Username is mandatory"],
         trim: true
       },
       email: {
         type: String,
-        required: [true, "Email required!"],
+        required: [true, "Email is mandatory"],
         trim: true
       },
       type: {
         type: String,
         trim: true,
         enum: {
-          values: ["Active", "Inactive"]
+          values: ["user", "admin"]
         },
-        default: "Active"
+        default: "user"
       },
       firstname: {
         type: String,
-        required: [false, "Author is required!"],
+        required: false,
         trim: true
       },
       lastname: {
         type: String,
-        required: [false, "Author is required!"],
+        required: false,
         trim: true
       },
       adress: {
         type: String,
-        required: [false, "Author is required!"],
+        required: false,
         trim: true
+      },
+      phoneNumber: {
+          type: String,
+          required: false,
+          trim: true
       }
     }
   );
