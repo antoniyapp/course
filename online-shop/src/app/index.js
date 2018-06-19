bb45d6c408d15fb2161f5ab5f1dc67db3a1b644e
antoniyapp <antoniya.partacheva@abv.bg>
2018-06-19 11:05:01 +0300

    bootstrap added

---
+++ online-shop/src/app/index.js
@@ -0,0 +1,10 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDom.render(
+<Router>
+    <App />
+ </Router>   ,
+document.getElementById('root'));
--- online-shop/src/vendor.js
+++ online-shop/src/vendor.js
@@ -3,6 +3,7 @@
 import 'react';
 import 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import $ from 'jquery';
+import Popper from 'popper.js';
 // Other vendors for example jQuery, Lodash or Bootstrap
 // You can import js, ts, css, sass, ...
